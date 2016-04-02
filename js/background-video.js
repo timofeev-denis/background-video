69f94db6f9f6621d43e450fd8735ac71c723d72f
Denis Timofeev <nakolenke@gmail.com>
2016-04-02 20:33:42 +0300

    Remove excess code and comments

--- js/background-video.js
+++ js/background-video.js
@@ -1,74 +1,34 @@
-/*
-document.addEventListener("DOMContentLoaded", function(event) { 
-	console.log( "DOMContentLoaded" );
-});
-*/
-
-var Item = function(name, formats, poster) {
-	if( name === undefined || formats === undefined ) {
-		return;
-	}
-	if( poster !== undefined ) {
-		this.poster = poster;
-	} else {
-		this.poster = name + ".jpg";
-	}
-	this.sources = new Array();
-	for(i = 0; i < formats.length; i++) {
-		this.sources[i] = {
-			src: name + "." + formats[i],
-			type: "video/" + formats[i]
-		};
-	}
-}
-
 function BackgroundVideo(container) {
     if(container === undefined) {
         this.container = document.body;
     } else {
         this.container = document.getElementById(container);
     }
-    /*
-    this.video = document.createElement("video");
-    this.video.setAttribute("id", "BackgroundVideoContainer");
-    this.video.className = "flexible";
-    this.video.autoplay = true;
-    this.video.loop = false;
-    this.video.muted = true;
-    this.playlist = new Array();
-    this.currentItem = -1;
-    this.videoCounter = 0;
-    //this.addToDOM();
-    console.log( "BackgroundVideo constructor" );
-    this.video.addEventListener("ended", this);
-    */
     this.currentItem = -1;
     this.videoCounter = 0;
     // All videos
     this.videos = new Array();
-    // Loaded videos
-    this.playlist = new Array();
 };
 
 BackgroundVideo.prototype = {
     
     // Implement the `EventListener` interface   
     handleEvent: function(event) {
-        console.log("EVENT: " + event.type + " " + event.target.id);
         switch (event.type) {
             case "ended": 
                 return this.playNextItem();
                 break;
             case "canplaythrough": 
-                for(i = 0; i < this.videos.length; i++) {
-                    if(this.videos[i].id == event.target.id) {
-                        console.log( " skip " + event.target.id );
-                        return false;
-                    }
+                if(this.videos[this.currentItem].readyState != 4) {
+                    return true;
                 }
-                //this.playlist.push(event.target);
-                console.log( " loaded " + event.target.id );
-                //console.log( this.playlist );
+                var i = this.currentItem;
+                do {
+                    i++;
+                } while(i < this.videos.length && this.videos[i].readyState == 4);
+                if( i < this.videos.length && this.videos[i].readyState != 4) {
+                    this.videos[i].load();
+                }
                 break;
         }
     },
@@ -103,12 +63,11 @@ BackgroundVideo.prototype = {
         var newVideo = document.createElement("video");
         this.initVideoTag(newVideo, name, formats, poster);
         newVideo.addEventListener("ended", this);
-        //newVideo.addEventListener("canplaythrough", this);
+        newVideo.addEventListener("canplaythrough", this);
         this.addToDOM(newVideo);
         this.videos.push(newVideo);
         if(this.videos.length == 1) {
-            // First video added - play it and put to playlist
-            //this.videos[0].play();
+            // First video added - play it
             this.playNextItem();
         }
     },
@@ -129,11 +88,8 @@ BackgroundVideo.prototype = {
         }
         var item = this.videos[this.currentItem];
         if(item.readyState == 4) { // 4 - HAVE_ENOUGH_DATA
-            console.log( "Playing item #" + this.currentItem );
             item.play();
         } else {
-            console.log( "Loading item #" + this.currentItem  + " & starting over");
-            item.load();
             this.currentItem = 0;
             this.videos[0].play();
         }
@@ -145,10 +101,15 @@ BackgroundVideo.prototype = {
     
 }
 
+/**
+Usage:
 
-
+// Create main object
 var bgv = new BackgroundVideo("myId");
-
+        
+// Add video (file name without extension, formats)
+bgv.addVideo( "video/Hello-World", [ "mp4", "ogv" ] );
 bgv.addVideo( "video/SampleVideo_1280x720_1mb", [ "mp4"] );
-bgv.addVideo( "video/Hello-World", [ "webm", "mp4", "ogv" ] );
 bgv.addVideo( "video/OneBigCircle-HD.mp4", [ "mp4" ] );
+
+ */
